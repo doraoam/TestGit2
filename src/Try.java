8ed82cf8310162b80f1620dc9782fa07d5dd63db
doraoam <doraoam2005@gmail.com>
2013-09-11 15:10:14 +0700

    ลองดูๆ

---
+++ src/Try.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: doraoam
+ * Date: 11/9/2556
+ * Time: 15:05 น.
+ * To change this template use File | Settings | File Templates.
+ */
+public class Try {
+}
