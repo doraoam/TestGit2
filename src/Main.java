608ac44b60f844e4711eaecd6b9fdc988716e407
doraoam <doraoam2005@gmail.com>
2013-09-11 14:42:19 +0700

    ตามพี่ไท2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 import java.util.Scanner;
 
-/**
- * Created with IntelliJ IDEA.
- * User: doraoam
- * Date: 11/9/2556
- * Time: 14:08 น.
- * To change this template use File | Settings | File Templates.
- */
 public class Main {
 
     public static void main(String[] args) {
